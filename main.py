1ff2c393cdcf02afc0234b6ed72925a0fe902a6a
travalja <alekstravalja0@gmail.com>
2025-10-08 09:51:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+
+
 # Funkcija za učitavanje teksta iz datoteke
 def ucitaj_tekst(filepath):
     try: 
@@ -16,10 +19,21 @@ def ocisti_tekst (tekst):
     interpunkcija = ['.', ',', '!', '?', ':', ';', '"', "'", '(', ')',]
     for znak in interpunkcija:
         tekst = tekst.replace(znak, '')
-
     lista_rijeci = tekst.split()
-    
     return lista_rijeci
+
+
+def broji_rijeci(lista_rijeci):
+    #
+    brojac_rijeci = {}
+    for rijec in lista_rijeci:
+        if rijec in brojac_rijeci:
+            brojac_rijeci[rijec] +=1
+        else:
+            brojac_rijeci[rijec] = 1
+    return brojac_rijeci
+
+
 
 if __name__== "__main__":
     filepath = "tekst.txt"
@@ -30,10 +44,15 @@ if __name__== "__main__":
         print(ucitani_tekst)
     else:
         print("Greska pri ucitavanju datoteke")
+    
     ucitani_tekst = ocisti_tekst(ucitani_tekst)
     if ucitani_tekst:
         print("Očišćeni tekst je:")
         print(ucitani_tekst)
+        #Brojanje rijeci i ispis rječnika
+        brojac_rijeci = broji_rijeci(ucitani_tekst)
+        print("Broj riječi u tekstu:")
+        print(brojac_rijeci)
     else:
         print ("Greška pri čišćenju teksta.")
 
